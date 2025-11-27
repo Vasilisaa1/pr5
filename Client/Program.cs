0b7939424eb54474abdb3923cfd8910444fad05f
vasilisaa <vasilisaveber7@gmail.com>
2025-11-27 23:30:24 +0500

    Метод OnSettings. Вебер

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -10,5 +11,57 @@ namespace Client
     public class Program
     {
         static IPAddress ServerIpAdress;
+        static int ServerPort;
+
+        static string ClientToken;
+        static DateTime ClientDateConnection;
+        static void Main(string[] args) {
+            OnSettings();
+        }
+        static void OnSettings() {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            string IpAddress = "";
+            if (File.Exists(Path))
+                {
+                    StreamReader streamReader = new StreamReader(Path);
+                IpAddress = streamReader.ReadLine();
+                    ServerIpAdress = IPAddress.Parse(IpAddress);
+                    ServerPort = int.Parse(streamReader.ReadLine());
+                    streamReader.Close();
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Server adress: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(IpAddress);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Server port: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(ServerPort.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Please provide the IP address if the license server: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    IpAddress = Console.ReadLine();
+                    ServerIpAdress = IPAddress.Parse(IpAddress);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Please specify the liecens server port: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    ServerPort = int.Parse(Console.ReadLine());
+
+                    StreamWriter streamWriter =new StreamWriter(Path);
+                    streamWriter.WriteLine(IpAddress);
+                    streamWriter.WriteLine(ServerPort.ToString());
+                    streamWriter.Close();
+                }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("To change, write the command: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("/config");
+
+        }
     }
 }
